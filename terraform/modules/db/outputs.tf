d5e599100b35eecc4c5da54a9532c994c3b5f1f9
restrry <restrry@gmail.com>
2017-10-02 20:01:49 +0200

    deploy via ansible

--- terraform/modules/db/outputs.tf
+++ terraform/modules/db/outputs.tf
@@ -1,1 +1,7 @@
+output "dp_external_ip" {
+  value = "${google_compute_instance.db.network_interface.0.access_config.0.assigned_nat_ip}"
+}
 
+output "dp_internal_ip" {
+  value = "${google_compute_instance.db.network_interface.0.address}"
+}
